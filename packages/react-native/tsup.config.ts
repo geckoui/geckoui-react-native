ba5028f7c693e683939d73308f4bc04a6d8d4ead
Wai Yan Phyo <oyhpnayiaw@gmail.com>
2026-04-26 14:41:50 +0700

    Use direct Typescript transpilation

--- packages/react-native/tsup.config.ts
+++ packages/react-native/tsup.config.ts
@@ -1,36 +1,19 @@
 import { defineConfig } from 'tsup';
 
-const external = [
-  'react',
-  'react-native',
-  'nativewind',
-  'class-variance-authority',
-  'tailwind-merge',
-];
-const esbuildOptions: Parameters<typeof defineConfig>[0]['esbuildOptions'] = (
-  options,
-) => {
-  options.jsx = 'automatic';
-  options.jsxImportSource = 'nativewind';
-};
-
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    dts: true,
-    clean: true,
-    external,
-    esbuildOptions,
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  external: [
+    'react',
+    'react-native',
+    'nativewind',
+    'class-variance-authority',
+    'tailwind-merge',
+  ],
+  esbuildOptions(options) {
+    options.jsx = 'automatic';
+    options.jsxImportSource = 'nativewind';
   },
-  {
-    entry: ['src/**/*.ts', 'src/**/*.tsx'],
-    outDir: 'dist/native',
-    format: ['esm'],
-    bundle: false,
-    dts: false,
-    clean: false,
-    external,
-    esbuildOptions,
-  },
-]);
+});
